7d80682403cf0bd9411ee682da8c52faf25b0b26
Raju <rajgopalreddy7382@gmail.com>
2025-09-20 12:21:19 +0530

    edit file

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,13 @@ function App() {
     // METHOD
     // DATA
 
-    // try {
-    //   let apiPath = `http://www.omdbapi.com/?apikey=c0a6198e&s=${searchWord}`
-    //   let apiResponse = await axios.get(apiPath)
-    //   setMoviesData([...apiResponse.data.Search])
-    // } catch (ex) {
-    //   alert('Site is under maintance ...')
-    // }
-    let apiPath = `http://www.omdbapi.com/?apikey=c0a6198e&s=${searchWord}`
-    let apiResponse = await axios.get(apiPath)
-    setMoviesData([...apiResponse.data.Search])
+    try {
+      let apiPath = `http://www.omdbapi.com/?apikey=c0a6198e&s=${searchWord}`
+      let apiResponse = await axios.get(apiPath)
+      setMoviesData([...apiResponse.data.Search])
+    } catch (ex) {
+      alert('Unable to process your Request ...')
+    }
   }
   return (
     <>
